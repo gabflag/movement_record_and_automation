6b534f7a248161d59bfd465c14019fec98441838
Gabflag <95552879+gabflag@users.noreply.github.com>
2023-10-27 17:09:19 -0300

    Update movement_record.py

--- movement_record.py
+++ movement_record.py
@@ -1,48 +1,54 @@
-import time
-import pyautogui
-from pynput.mouse import Listener
+from pynput import mouse
+from pynput.mouse import Controller
 
 def on_move(x, y):
+    move = 'Pointer moved to {0}'.format((x, y))
     with open('mouse_log.txt', 'a') as file:
-        file.write(f"Mouse movement: ({x}, {y})\n")
+        file.write(f"{move}\n")
 
 def on_click(x, y, button, pressed):
-    action = "Pressed" if pressed else "Released"
+    action = 'Pressed' if pressed else 'Released'
+    click_info = '{0} at {1}'.format(action, (x, y))
+
     with open('mouse_log.txt', 'a') as file:
-        file.write(f"{action} - Button {button} at ({x}, {y})\n")
+        file.write(f"{click_info}\n")
 
 def on_scroll(x, y, dx, dy):
+    scroll_info = 'Scrolled {0} at {1}'.format('down' if dy < 0 else 'up', (x, y))
     with open('mouse_log.txt', 'a') as file:
-        file.write(f"Mouse scroll at ({x}, {y}): ({dx}, {dy})\n")
+        file.write(f"{scroll_info}\n")
 
-def automatic_movement():
-    with open('mouse_log.txt', 'r') as file:
+def replay_mouse_events_from_file(filename):
+    mouse_controller = Controller()
+    
+    with open(filename, 'r') as file:
         lines = file.readlines()
 
     for line in lines:
-        parts = line.strip().split(' - ')
-        if "Mouse movement" in parts[0]:
-            coords = parts[0].split(":")[1].strip()[1:-1].split(',')
-            x, y = int(coords[0]), int(coords[1])
-            pyautogui.moveTo(x, y)
-        elif "Pressed" in parts[0]:
-            coords = parts[1].split(" at ")[1].strip()[1:-1].split(',')
-            x, y = int(coords[0]), int(coords[1])
-            pyautogui.click(x, y)
+        line = line.strip()
+        if 'Pointer moved to' in line:
+            _, coords = line.split('to ')
+            x, y = map(int, coords.strip('()').split(', '))
+            mouse_controller.position = (x, y)
 
-        elif "Mouse scroll" in parts[0]:
-            values = parts[1].strip()[1:-1].split(':')
-            dx, dy = int(values[0]), int(values[1])
-            pyautogui.scroll(dx, dy)
-        else:
-            print('Error')
+        elif 'Pressed' in line or 'Released' in line:
+            if 'Pressed' in line:
+                mouse_controller.press(mouse.Button.left)
+            else:
+                mouse_controller.release(mouse.Button.left)
 
-        time.sleep(0.1)
+        elif 'Scrolled' in line:
+            if 'down' in line:
+                mouse_controller.scroll(0, -1)
+            else:
+                mouse_controller.scroll(0, 1)
 
-def record_movement():
-    # Record mouse movement
-    with Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as listener:
+
+def gravar():
+    with mouse.Listener(
+            on_move=on_move,
+            on_click=on_click,
+            on_scroll=on_scroll) as listener:
         listener.join()
 
-# record_movement()
-# automatic_movement()
+replay_mouse_events_from_file('mouse_log.txt')
